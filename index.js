c2550960910fc3d088543d9e994b087bbb455703
tho hem <duhs.lethys@gmail.com>
2019-01-04 23:21:17 +0100

    test

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
-
+var fs = require('fs');
 var morgan = require('morgan');
+var path = require('path');
 
-app.use(morgan(':date[iso] - :url'));
+var stream = fs.createWriteStream(path.join(__dirname,'access.log'),{flags: 'a'});
+
+app.use(morgan(':date[iso] - :url', {stream: stream}));
 
 app.use('/', function(req, res){
 	res.send('yo');
 });
 
 app.listen(port);
-console.log('Server running on port: ' + port);
+console.log('Server running on port: ' + port)
