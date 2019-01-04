8c64aec6bc273e22859d2b3106f13e96f5742d15
tho hem <duhs.lethys@gmail.com>
2019-01-04 10:36:45 +0100

    latest V

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-var http = require('http-debug').http;
-var https = require('http-debug').https;
- 
-https.debug = 2;
- 
-/****
- * debug states
- * - 0     : off     (default state)
- * - 1     : on      (show request, write, end)
- * - 2     : verbose (on + error and socket event reporting)
- *
- * Also support `process.env.HTTP_DEBUG` at load time, which
- * will overide default state, however, anything passed via
- * `http.debug` at run time will cancel out
- * `process.env.HTTP_DEBUG`.
- ****/
- 
-// Make http requests as usual.
-https.get('https://safe-citadel-91364.herokuapp.com/', function (err, res) {
-   if (err) console.trace(err);
-   console.log(res.statusCode);
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+// use combined preset, see https://github.com/expressjs/morgan#combined
+app.use(morgan('combined'));
+
+app.get('/', (req, res)  => {
+  res.send('Hello from Express.js server!')
+});
+
+
+const PORT = 3000;
+
+// run the server
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
