73f2c7552441e03428953e8b2d4d07a10a79424c
tho hem <duhs.lethys@gmail.com>
2019-01-04 10:26:02 +0100

    new vers

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
-var https = require('https');
-var http = require('http');
-var fs = require('fs');
-
-const options = {
-                key: fs.readFileSync(__dirname+'/alt1-key.pem'),
-                cert: fs.readFileSync(__dirname+'/alt1-cert.pem')
-};
-
-https.createServer(options, function(req, res){
-                console.log('req made: '+req.url);
-                res.setHeader('Access-Control-Allow-Origin', '*');
-                res.setHeader('Access-Control-Request-Method', '*');
-                res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-                res.writeHeader(200,{'Content-Type': 'text/plain'});
-                var read = fs.createReadStream(__dirname+'/Apps/stream.czml', 'utf8');
-                read.pipe(res);
-}).listen(443);
+var http = require('http-debug').http;
+var https = require('http-debug').https;
  
-var server = http.createServer(function(req, res){
-                console.log('req made: '+req.url);
-                res.setHeader('Access-Control-Allow-Origin', '*');
-                res.setHeader('Access-Control-Request-Method', '*');
-                res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-                res.writeHeader(200,{'Content-Type': 'text/plain'});
-                var read = fs.createReadStream(__dirname+'/Apps/stream.czml', 'utf8');
-                read.pipe(res);
+https.debug = 2;
+ 
+/****
+ * debug states
+ * - 0     : off     (default state)
+ * - 1     : on      (show request, write, end)
+ * - 2     : verbose (on + error and socket event reporting)
+ *
+ * Also support `process.env.HTTP_DEBUG` at load time, which
+ * will overide default state, however, anything passed via
+ * `http.debug` at run time will cancel out
+ * `process.env.HTTP_DEBUG`.
+ ****/
+ 
+// Make http requests as usual.
+https.get('https://safe-citadel-91364.herokuapp.com/', function (err, res) {
+   if (err) console.trace(err);
+   console.log(res.statusCode);
 });
-
-server.listen(8000);
