89c945778c9c1cdad19d0ebb869fc5ebd7b3de9c
tho hem <duhs.lethys@gmail.com>
2019-01-04 11:30:23 +0100

    safd

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 const express = require('express');
 const logger = require('morgan');
-const app = express();
 var port = process.env.PORT || 8080;
 var fs = require('fs');
+var path = require('path');
 
+var app = express();
 //app.listen(port, function() {
 //	console.log("app runs on ip:"+port);
 //});
 
 app.enable("trust proxy");
 
-var accessLogStream = fs.createWriteStream('./access.log', {flags: 'a'});
-app.use(logger({format:':date[iso] :remote-addr :method :url :status :res[content-length] - :response-time ms',stream: {
-	write: function(str){
-		accessLogStream.write(str);
-		console.log(str);
-	}
-}}));
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'});
+app.use(logger(':date[iso] :remote-addr :method :url :status :res[content-length] - :response-time ms',{stream: accessLogStream}));
 
-app.get('/', (req, res)  => {
+app.get('/', (req, res) => {
   res.send('Hello from Express.js server!')
 });
 
