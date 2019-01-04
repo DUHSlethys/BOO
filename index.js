c277f031ad22abcd5edf9b2677b227d5c912583e
tho hem <duhs.lethys@gmail.com>
2019-01-04 10:45:40 +0100

    yea

--- index.js
+++ index.js
@@ -3,17 +3,18 @@ const morgan = require('morgan');
 
 const app = express();
 
+app.enable("trust proxy");
 // use combined preset, see https://github.com/expressjs/morgan#combined
-app.use(morgan('combined'));
+app.use(morgan('date[ISO] :remote-addr :method :url :status'));
 
 app.get('/', (req, res)  => {
   res.send('Hello from Express.js server!')
 });
 
 
-const PORT = 3000;
+//const PORT = 3000;
 
 // run the server
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+//app.listen(PORT, () => {
+//  console.log(`Example app listening on port ${PORT}!`);
+//});
